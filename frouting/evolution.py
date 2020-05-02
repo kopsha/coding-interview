04ba16eea646940dc58f5f1a6d5b4fb4544844c6
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2020-05-02 11:03:29 +0300

    save again

--- frouting/evolution.py
+++ frouting/evolution.py
@@ -15,8 +15,10 @@ from random import shuffle
 from operator import mul
 from functools import reduce
 import math
+import random
 
 import statistics
+import pathfinding
 
 
 def print_stage(text, row_size=80):
@@ -27,9 +29,7 @@ def print_stage(text, row_size=80):
 
 
 def mystr(x):
-    if type(x) is list:
-        x = str(x)
-    return '  .' if x == None else f'{x:>3}'
+    return '  .' if x == None else f'{x:5.0f}'
 
 
 def print_array(a):
@@ -92,14 +92,20 @@ def scan_target_set(data):
 
 
 fig_counter = 0
-def save_figure(layout):
+def save_figure(layout, name_tag=''):
     global fig_counter
     fig_counter += 1
     data, rows, cols = layout
     fig, ax0 = plt.subplots(1)
     ax0.matshow(data)
-    fig.savefig(f'solutions/evo_data_{fig_counter:>03}.png')
+    fig.savefig(f'solutions/evo_{fig_counter:>03}_{name_tag}.png')
     plt.close()
+
+    # print(f'solutions/evo_{fig_counter:>03}_{name_tag}.png')
+    # print_array(data)
+
+    return
+
 
 def scan_target_nodes(layout, targets):
     data, rows, cols = layout
@@ -151,12 +157,11 @@ def alternator(left, right):
         yield left if count % 2 == 0 else right
         count += 1
 
-
 def distance(left, right):
     left_row, left_col = left
     right_row, right_col = right
-    dist = ((right_row - left_row) ** 2 + (right_col - left_col) ** 2)
-    return int(dist)
+    dist = (right_row - left_row) ** 2 + (right_col - left_col) ** 2
+    return dist
 
 
 def nearest_target(node, target_nodes):
@@ -164,29 +169,13 @@ def nearest_target(node, target_nodes):
 
     distances = []
     for tk, tnodes in target_nodes.items():
-        dd = [ distance(node, tnode) for tnode in tnodes ]
-        dd.sort()
-        middle = int(float(len(dd)) / 1.6180339887498948482)
-        dd = dd[:middle+1]
-        distances.append( (tk, reduce(mul, dd, 1)) )
+        dd = [distance(node, tnode) for tnode in tnodes]
+        distances.append( (tk, statistics.harmonic_mean(dd)) )
 
     distances.sort(key=lambda pair: pair[1])
 
     value, dist = distances[0]
     return value, dist
-
-def grab_same_nodes(layout, value):
-    data, rows, cols = layout
-
-    nodes = []
-
-    for i, row in enumerate(data):
-        for j, v in enumerate(row):
-            node = (i, j)
-            if v == value:
-                nodes.append(node)
-
-    return nodes
 
 def group_nodes_in_regions(layout, nodes):
     region_id = 0
@@ -212,20 +201,22 @@ def group_nodes_in_regions(layout, nodes):
     return regions
 
 
+def value_of(layout, node):
+    data, rows, cols = layout
+    i, j = node
+    return data[i][j] if (0 <= i < rows) and (0 <= j < rows) else None
 
 
-def search_solution(layout, target, target_nodes):
-    import pathfinding
-
-    def get_left_node(data, node, target):
+def search_solution(layout, sector_layout, target, target_nodes):
+    def connected_left_node(data, node):
         i, j = node
         j -= 1
-        return (i, j) if (j >= 0) and (data[i][j] == target) else None
+        return (i, j) if (j >= 0) and (data[i][j] == target or type(data[i][j]) is float) else None
 
-    def get_top_node(data, node, target):
+    def connected_top_node(data, node):
         i, j = node
         i -= 1
-        return (i, j) if (i >= 0) and (data[i][j] == target) else None
+        return (i, j) if (i >= 0) and (data[i][j] == target or type(data[i][j]) is float) else None
 
     mdata, rows, cols = layout
 
@@ -235,34 +226,128 @@ def search_solution(layout, target, target_nodes):
     for i, row in enumerate(mdata):
         for j, value in enumerate(row):
             node = (i, j)
-            if value == target:
+            if value == target or type(value) is float:
                 vertices.append(node)
-                left_node = get_left_node(mdata, node, target)
-                if left_node is not None:
-                    edges.append( (left_node, node, 1) )
-                top_node = get_top_node(mdata, node, target)
-                if top_node is not None:
-                    edges.append( (top_node, node, 1) )
-
+                left_node = connected_left_node(mdata, node)
+                if left_node in vertices:
+                    delta = 1 if value_of(sector_layout, left_node) == value else value
+                    edges.append( (left_node, node, delta) )
+                    edges.append( (node, left_node, delta) )
+                top_node = connected_top_node(mdata, node)
+                if top_node in vertices:
+                    delta = 1 if value_of(sector_layout, top_node) == value else value
+                    edges.append( (top_node, node, value) )
+                    edges.append( (node, top_node, value) )
 
     g = pathfinding.GraphUndirectedWeighted( len(vertices) )
     for edge in edges:
         left, right, dist = edge
         g.add_edge( vertices.index(left), vertices.index(right), dist )
-        g.add_edge( vertices.index(right), vertices.index(left), dist )
 
+    solution = []
     t_nodes = target_nodes[target]
-    for left, right in zip(t_nodes[:-1], t_nodes[1:]):    
-        path, distance = pathfinding.dijkstra(g, vertices.index(left), vertices.index(right))
-        if distance == float('inf'):
-            print('no route found for target', target, ', nodes', left, right)
-        else:
-            for ndx, node in enumerate(vertices):
-                i, j = node
-                if ndx in path:
-                    mdata[i][j] = target
-                else:
-                    mdata[i][j] = 0
+    for left, right in zip(t_nodes[:-1], t_nodes[1:]):
+        print(f'solving target {target}', 'nodes: ', left, right)
+        path, dist = pathfinding.dijkstra(g, vertices.index(left), vertices.index(right))
+        if dist == float('inf'):
+            #print('\tno route found for target', target, ', nodes', left, right)
+            path.clear()
+
+        for ndx in path:
+            solution.append( vertices[ndx] )
+
+    return solution
+
+
+def apply_target_spectrum(layout, target_nodes):
+    data, rows, cols = layout
+    mdata = copy.deepcopy(data)
+
+    for i, row in enumerate(mdata):
+        for j, value in enumerate(row):
+            node = (i, j)
+            if value == 0:
+                nt_val, dist = nearest_target(node, target_nodes)
+                mdata[i][j] = nt_val
+    return mdata
+
+
+def apply_dist_spectrum(layout, target_nodes):
+    data, rows, cols = layout
+    mdata = copy.deepcopy(data)
+
+    max_dist = 0
+    for i, row in enumerate(mdata):
+        for j, value in enumerate(row):
+            node = (i, j)
+            if value == 0:
+                nt_val, dist = nearest_target(node, target_nodes)
+                mdata[i][j] = float(dist)
+                max_dist = dist if dist > max_dist else max_dist
+
+    for i, row in enumerate(mdata):
+        for j, value in enumerate(row):
+            node = (i, j)
+            if type(value) is float:
+                mdata[i][j] = 1 + (1.0 - value/max_dist) * 100
+
+    return mdata
+
+
+def apply_marks_for_target(layout, target, target_nodes):
+    mdata, rows, cols = layout
+
+    for i, row in enumerate(mdata):
+        for j, value in enumerate(row):
+            node = (i, j)
+            if type(value) is float:
+                bl = boxed_labels(layout, node)
+                nbs = set(bl)
+                for nb in nbs:
+                    if type(nb) is int and nb > 0 and nb != target:
+                        mdata[i][j] = TEMP_WALL_MARK
+                        break
+
+    return mdata
+
+
+def scan_connected_targets(layout, target_nodes):
+    data, rows, cols = layout
+
+    connected_targets = []
+
+    for target in target_nodes:
+        t_nodes = target_nodes[target]
+
+        visited = set()
+        queue = deque()
+        queue.append(t_nodes[0])
+        while len(queue):
+            vnode = queue.popleft()
+            visited.add(vnode)
+            nbs = [
+                n
+                for n in crossed_nodes(layout, vnode)
+                if data[n[0]][n[1]] == target and \
+                   n not in visited and \
+                   n not in queue
+            ]
+            queue.extend(nbs)
+
+        not_visited_targets = set(t_nodes) - visited
+        if len(not_visited_targets) == 0:
+            connected_targets.append(target)
+
+    return connected_targets
+
+
+def mark_solutions(mlayout, solutions):
+    mdata, rows, cols = mlayout
+
+    for k in solutions:
+        for step in solutions[k]:
+            i, j = step
+            mdata[i][j] = k
 
     return mdata
 
@@ -270,60 +355,67 @@ def search_solution(layout, target, target_nodes):
 def evolve(layout, target_nodes):
     data, rows, cols = layout
 
-    updates = defaultdict(list)
-
-    for i, row in enumerate(data):
-        for j, value in enumerate(row):
-            node = (i, j)
-            if value == 0:
-                nt_val, dist = nearest_target(node, target_nodes)
-                nbs = set(boxed_labels(layout, node)) - {0, TEMP_WALL_MARK, WALL_MARK}
-                if len(nbs) > 1:
-                    updates[node].append( TEMP_WALL_MARK )
-                else:
-                    updates[node].append( nt_val )
-
-
-    # perform updates
-    mdata = copy.deepcopy(data)
-    for node, update in updates.items():
-        i, j = node
-        mdata[i][j] = update.pop()
-
-
-    for target in [1, 2, 3, 4, 5]:
+    repeat = True
+    solved_targets = set()
+    while repeat:
+        whaaat = {}
+        repeat = False
+        mdata = copy.deepcopy(data)
         mlayout = (mdata, rows, cols)
-        mdata = search_solution(mlayout, target, target_nodes)
+        cost_data = apply_dist_spectrum(mlayout, target_nodes)
+        sector_data = apply_target_spectrum(mlayout, target_nodes)
 
+        cost_layout = (cost_data, rows, cols)
+        #save_figure(cost_layout, name_tag='cost')
 
-    for i, row in enumerate(mdata):
-        for j, value in enumerate(row):
-            node = (i, j)
-            if value == TEMP_WALL_MARK:
-                mdata[i][j] = 0
+        sector_layout = (sector_data, rows, cols)
+        #save_figure(sector_layout, name_tag='sectors')
 
-    return mdata
+        for t in target_nodes:
+            if t in solved_targets:
+                continue
+            mdata = copy.deepcopy(cost_data)
+            mlayout = (mdata, rows, cols)
+            mdata = apply_marks_for_target(mlayout, t, target_nodes)
+            #save_figure(mlayout, name_tag=f'marks_for_{t}')
 
-def evolve_back(layout, target_nodes):
-    data, rows, cols = layout
+            solution = search_solution(mlayout, sector_layout, t, target_nodes)
 
-    updates = defaultdict(list)
+            # restore mdata
+            mdata = copy.deepcopy(data)
+            mlayout = (mdata, rows, cols)
 
-    for i, row in enumerate(data):
-        for j, value in enumerate(row):
-            node = (i, j)
-            if value == 0:
-                nt_val, dist = nearest_target(node, target_nodes)
-                updates[node].append( nt_val )
+            if solution:
+                 whaaat[t] = solution
 
-    # perform updates
-    mdata = copy.deepcopy(data)
-    for node, update in updates.items():
-        i, j = node
-        mdata[i][j] = update.pop()
+        # select the shortest solution
+        if len(whaaat) > 0:
+            sol_paths = [(t,len(x)) for t, x in whaaat.items()]
+            sol_paths.sort(key=lambda k: k[1])
+            t, sol = random.choice(sol_paths)
+            #t, sol = sol_paths[0]
+            whaaat = { t: whaaat[t] }
+            mdata = mark_solutions(mlayout, whaaat)
+        else:
+            return mdata, False
 
-    return mdata
+        connex = scan_connected_targets(mlayout, target_nodes)
+        for t in connex:
+            if t not in solved_targets:
+                solved_targets.add(t)
+                open_targets = set(target_nodes.keys()) - solved_targets
+                repeat = len(open_targets) > 0
 
+                print(f'new pad solved {t} only {len(open_targets)} remaining.')
+                save_figure(mlayout, name_tag=f'new_pad')
+
+                if repeat:
+                    target_nodes = {k:target_nodes[k].copy() for k in open_targets}
+                
+                    data = copy.deepcopy(mdata)
+                    layout = (data, rows, cols)
+
+    return mdata, len(whaaat.keys()) == len(target_nodes.keys())
 
 WALL_MARK = -1
 TEMP_WALL_MARK = -2
@@ -366,11 +458,17 @@ def main():
         targets = scan_target_set(data)
         target_nodes = scan_target_nodes(layout, targets)
 
-        data = evolve(layout, target_nodes)
+        solved = False
+        cnt = 0
+        while not solved and cnt < 100:
+            mdata, solved = evolve(layout, target_nodes)
+            data = copy.deepcopy(mdata)
+            cnt += 1
 
-        fig, (ax0, ax1) = plt.subplots(1, 2)
-        ax0.matshow(data)
-        ax1.matshow(solved_data)
+        fig, (ax0, ax1, ax2) = plt.subplots(1, 3)
+        ax0.matshow(original_data)
+        ax1.matshow(data)
+        ax2.matshow(solved_data)
         fig.savefig(f'solutions/evo_{filename.rstrip(".csv")}.png')
 
 
